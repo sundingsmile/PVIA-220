6b5f11612a1f3f93da9582d00ec7fcea88e40e47
Smile <sundingsmile@163.com>
2023-09-08 15:42:20 +0800

    修复test_a报错和断言方式

--- TestCase/test_practice.py
+++ TestCase/test_practice.py
@@ -60,7 +60,7 @@ class TestB():
     # @pytest.mark.parametrize('a,b',[(1,1)])
     def test_a(self):
         print('test_a')
-        assert 1==2
+        assert 3==2
 
     @allure.step('Jenkins测试')
     def test_b(self,request):
